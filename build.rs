7ea449807ca3310da37579178614cd2397626b00
Nebula <me@itsnebula.net>
2023-06-17 18:55:00 -0400

    chore: remove PowerShell completions

--- build.rs
+++ build.rs
@@ -3,7 +3,7 @@ mod cli;
 
 use std::{env, io};
 
-use clap_complete::shells::{Bash, Elvish, Fish, PowerShell, Zsh};
+use clap_complete::shells::{Bash, Elvish, Fish, Zsh};
 use clap_complete_fig::Fig;
 
 fn main() -> io::Result<()> {
@@ -21,7 +21,6 @@ fn generate_completions() -> io::Result<()> {
     clap_complete::generate_to(Elvish, cli, BIN_NAME, OUT_DIR)?;
     clap_complete::generate_to(Fig, cli, BIN_NAME, OUT_DIR)?;
     clap_complete::generate_to(Fish, cli, BIN_NAME, OUT_DIR)?;
-    clap_complete::generate_to(PowerShell, cli, BIN_NAME, OUT_DIR)?;
     clap_complete::generate_to(Zsh, cli, BIN_NAME, OUT_DIR)?;
 
     Ok(())
